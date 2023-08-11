2ae33ccc9ba081c453cf96e34fe02873d148fadf
Demidov, Yevgeny <Yevgeny.Demidov@nedra.digital>
2023-08-11 12:51:14 +0300

    add gitignore

--- src/test/java/tests/UiElementTests.java
+++ src/test/java/tests/UiElementTests.java
@@ -5,12 +5,10 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
 import static io.qameta.allure.Allure.step;
 
-public class uiElementTests extends TestBase{
+public class UiElementTests extends TestBase {
 	@Test
 	@Tag("ios")
 	void uiElements() {
